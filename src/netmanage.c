1a5825450f104cb1d33431a27989c43c97435b1c
David le Blanc <david.leblanc@freestyletechnology.com.au>
2015-08-03 17:06:08 +1000

    Initial commit

---
+++ src/netmanage.c
@@ -0,0 +1,37 @@
+/*
+ * File: netmanage.c
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#include "netmanage.h"
+#include "config.h"
+
+
+int
+main(int ac, char *av[])
+{
+	return 1;
+}
